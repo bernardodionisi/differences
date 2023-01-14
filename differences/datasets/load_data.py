1ab7120b167f14651f9b2303133b1112ab8d5101
Bernardo Dionisi <bernardo.dionisi@gmail.com>
2023-01-14 12:26:22 -0500

    TST: replace rpy2 calls with precalculated results from R
    modify tests to compare results from differences' ATTgt and R's did.

--- differences/datasets/load_data.py
+++ differences/datasets/load_data.py
@@ -5,13 +5,18 @@ from os.path import abspath, join, split
 import numpy as np
 from pandas import qcut, read_csv, to_datetime
 
+__all__ = ["rc_two_periods", "rc_multi_period", "mpdta", "simulated_data"]
 
-def rc_two_periods(py_format=True):
+
+def rc_two_periods(py_format: bool = True, seed: int = 4321):
     """two-period repeated cross section, sim_rc santanna zhao"""
     data = _load_data(__file__, "rcs-two_periods.csv")
 
     data["cohort"] = np.where(data["d"], 2000, np.nan)
     data["time"] = np.where(data["post"], 1999, 2000)
+
+    np.random.seed(seed)
+    data["random_weights"] = np.random.uniform(low=0, high=1, size=len(data))
 
     if py_format:
         data.set_index(["id", "time"], inplace=True)
@@ -26,9 +31,12 @@ def rc_two_periods(py_format=True):
     }
 
 
-def rc_multi_period(py_format=True):
+def rc_multi_period(py_format: bool = True, seed: int = 4321):
     """multi-period repeated cross section, sim_rc santanna zhao"""
     data = _load_data(__file__, "rcs-multi_period.csv")
+
+    np.random.seed(seed)
+    data["random_weights"] = np.random.uniform(low=0, high=1, size=len(data))
 
     if py_format:
         data["G"] = np.where(data["G"] == 0, np.nan, data["G"])
@@ -40,13 +48,19 @@ def rc_multi_period(py_format=True):
         "time_name": "period",
         "cohort_name": "G",
         "y_name": "Y",
+        "panel": False,
+        "allow_unbalanced_panel": False,
+        "weights_name": "random_weights",
     }
 
 
-def mpdta(py_format=True):
+def mpdta(py_format: bool = True, seed: int = 4321):
     """multi-period balanced panel"""
 
     data = _load_data(__file__, "mpdta.csv")
+
+    np.random.seed(seed)
+    data["random_weights"] = np.random.uniform(low=0, high=1, size=len(data))
 
     if py_format:
         data["first_treat"] = np.where(
@@ -60,18 +74,28 @@ def mpdta(py_format=True):
         "time_name": "year",
         "cohort_name": "first_treat",
         "y_name": "lemp",
+        "panel": True,
+        "allow_unbalanced_panel": False,
     }
 
 
 # ----------------------------------------------------------------------
 
 
-def simulated_data(py_format=True, single_event=True, separate_cohort_data=False):
+def simulated_data(
+        py_format: bool = True,
+        single_event: bool = True,
+        separate_cohort_data: bool = False,
+        seed: int = 4321,
+):
     if single_event:
         data_name = "se"
     else:
         data_name = "me"
     data = _load_data(__file__, f"{data_name}.csv")
+
+    np.random.seed(seed)
+    data["random_weights"] = np.random.uniform(low=0, high=1, size=len(data))
 
     data = data.assign(
         T=lambda x: to_datetime(x["T"]),
---
+++ differences/tests/_attgt.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+pd.set_option("display.max_rows", 6)
+
+from differences import load_data, ATTgt
+from differences.tests._utility import get_r_did_result
+
+data_name = "rc_multi_period"
+
+params_tuple = (0, 'varying', 'nevertreated', 'dr-mle', 'random_weights')
+anticipation, base_period, control_group, est_method, weights_name = params_tuple
+
+R_result = get_r_did_result(
+    data_name=data_name,
+    params_tuple=params_tuple,
+    result_type="att_gt",
+)
+
+data_dict = getattr(load_data, data_name)()
+
+att_gt = ATTgt(
+    data=data_dict['data'],
+    cohort_name=data_dict["cohort_name"],
+    anticipation=anticipation,
+    base_period=base_period,
+)
+
+att_gt.fit(
+    formula=f"{data_dict['y_name']}",
+    control_group=control_group,
+    weights_name=weights_name,
+    est_method=est_method,
+)
+
+agg_type = "time"
+att_gt.aggregate(agg_type, overall=True)
+
+get_r_did_result(
+    data_name=data_name,
+    params_tuple=params_tuple,
+    result_type="overall",
+    agg_type=agg_type
+)
